0d5c04e323b2a3a18aee92ff0ad42237d7929443
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-12-02 20:44:20 +0500

    done

--- get_post_weekday.py
+++ get_post_weekday.py
@@ -1,5 +1,8 @@
 from read_data import fromJson
-def get_post_weekday(data:dict)->dict:
+import datetime
+
+
+def get_post_weekday(data: dict) -> dict:
     """
     Return the number of posts for each weekday
     args:
@@ -8,11 +11,21 @@ def get_post_weekday(data:dict)->dict:
     """
     # Initialize a counter
     count = {}
+    messages = data['messages']
+    wd = {}
 
     # Loop through the dictionary
-    messages= data['messages']
+    for day in range(0, 7):
+        count[day] = 0
+        for m in messages:
+            d = m['date']
+            format_str = '%Y-%m-%dT%H:%M:%S'
+            date = datetime.datetime.strptime(d, format_str)
+            if m['type'] == 'message':
+                count[day] += date.weekday() == day
 
     return count
+
 
 # Path of the file to read
 file_path = "data/result.json"
